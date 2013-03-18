d54688b9b78e77db5bd196cdfd2887f726c08c20
fschwiet <fschwiet@gmail.com>
2013-03-18 14:23:17 -0700

    can load credentials from file

--- OmnitureConsole/SavedApiCredentials.cs
+++ OmnitureConsole/SavedApiCredentials.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using ManyConsole;
@@ -11,10 +12,42 @@ namespace OmnitureConsole
         public string Username;
         public string Secret;
 
+        public SavedApiCredentials()
+        {
+            var path = Path.GetFullPath(".");
+            var root = Path.GetPathRoot(path);
+
+            while (path != root)
+            {
+                Console.Write("trying " + path);
+                var credentialsPath = Path.Combine(path, "omniture-api-secret.txt");
+                if (File.Exists(credentialsPath))
+                {
+                    var lines = File.ReadAllLines(credentialsPath).Where(l => !string.IsNullOrEmpty(l)).ToArray();
+
+                    Username = lines[0];
+                    Secret = lines[1];
+                    break;
+                }
+                else
+                {
+                    path = new DirectoryInfo(path).Parent.FullName;
+                }
+            }
+        }
+
         public void ApplyTo(ConsoleCommand command)
         {
-            command.HasRequiredOption("u=", "Username", v => Username = v);
-            command.HasRequiredOption("s=", "Secret key", v => Secret = v);
+            if (string.IsNullOrEmpty(Username))
+            {
+                command.HasRequiredOption("u=", "Username", v => Username = v);
+                command.HasRequiredOption("s=", "Secret key", v => Secret = v);
+            }
+            else
+            {
+                command.HasOption("u=", string.Format("Username (default is {0})", Username), v => Username = v);
+                command.HasOption("s=", string.Format("Secret key (default is for {0})", Username), v => Secret = v);
+            }
         }
 
         public override string ToString()
