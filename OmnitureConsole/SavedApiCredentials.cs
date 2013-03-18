eb4960b79b14515b2da9536f95faa3dbfd4116c8
fschwiet <fschwiet@gmail.com>
2013-03-18 13:58:13 -0700

    extract credentials to a class

---
+++ OmnitureConsole/SavedApiCredentials.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ManyConsole;
+
+namespace OmnitureConsole
+{
+    public class SavedApiCredentials
+    {
+        public string Username;
+        public string Secret;
+
+        public void ApplyTo(ConsoleCommand command)
+        {
+            command.HasRequiredOption("u=", "Username", v => Username = v);
+            command.HasRequiredOption("s=", "Secret key", v => Secret = v);
+        }
+
+        public override string ToString()
+        {
+            return Username ?? "<null>";
+        }
+    }
+}
--- OmnitureConsole/TestCommand.cs
+++ OmnitureConsole/TestCommand.cs
@@ -10,21 +10,21 @@ namespace OmnitureConsole
 {
     public class TestCommand : ConsoleCommand
     {
-        public string Username;
-        public string Secret;
+        public SavedApiCredentials Credentials;
         public string Endpoint = "https://api.omniture.com/admin/1.3/";
 
         public TestCommand()
         {
             this.IsCommand("test-api-access", "Visit https://developer.omniture.com/en_US/content_page/enterprise-api/c-get-web-service-access-to-the-enterprise-api to setup API access for a user and to find your API secret key.");
-            this.HasRequiredOption("u=", "Username", v => Username = v);
-            this.HasRequiredOption("s=", "Secret key", v => Secret = v);
             this.HasOption("e=", "Endpoint url used for requests.", v => Endpoint = v);
+
+            Credentials = new SavedApiCredentials();
+            Credentials.ApplyTo(this);
         }
 
         public override int Run(string[] remainingArguments)
         {
-            var client = ClientHelper.GetClient(Username, Secret, Endpoint);
+            var client = ClientHelper.GetClient(Credentials.Username, Credentials.Secret, Endpoint);
 
             //https://developer.omniture.com/en_US/documentation/omniture-administration/r-getreportsuites
             simple_report_suites_rval results = client.CompanyGetReportSuites(new[] {"standard"}, "");
